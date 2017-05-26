725fd14b7dcdee345998929ac09b8b597e556cfa
Marcel Widmer <marzelwidmer@gmail.com>
2017-05-26 22:53:00 +0200

    initial commit

---
+++ src/main/java/ch/keepcalm/web/bloodtypeapp/BloodTypeAppApplication.java
@@ -0,0 +1,12 @@
+package ch.keepcalm.web.bloodtypeapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodTypeAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodTypeAppApplication.class, args);
+	}
+}
