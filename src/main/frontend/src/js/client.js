ef5043e404868fc0df8cf9b72a61ff96395ca407
Marcel Widmer <marzelwidmer@gmail.com>
2017-05-27 18:15:04 +0200

    sample how to work with constructors

--- src/main/frontend/src/js/client.js
+++ src/main/frontend/src/js/client.js
@@ -3,11 +3,17 @@ import ReactDOM from 'react-dom';
 
 //import Layout from "./components/Layout";
 class Layout extends React.Component {
+  constructor(){
+    super();
+    this.name = "Will";
+  }
+
   getVal(val) {
     return 'Will is ' + val;
   }
   render() {
-    return <h1> It works! {this.getVal(3+2)}</h1>;
+    //return <h1> It works! {this.getVal(3+2)}</h1>;
+    return <h1> It works {this.name} ! </h1>;
   }
 }
 
