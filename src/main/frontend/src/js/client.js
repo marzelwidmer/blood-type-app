ea90f0c98c8c0c194f23c1af9f8ae53f72620d04
Marcel Widmer <marzelwidmer@gmail.com>
2017-05-27 18:44:56 +0200

    Composing Multiple React.js Components

--- src/main/frontend/src/js/client.js
+++ src/main/frontend/src/js/client.js
@@ -1,21 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-//import Layout from "./components/Layout";
-class Layout extends React.Component {
-  constructor(){
-    super();
-    this.name = "Will";
-  }
-
-  getVal(val) {
-    return 'Will is ' + val;
-  }
-  render() {
-    //return <h1> It works! {this.getVal(3+2)}</h1>;
-    return <h1> It works {this.name} ! </h1>;
-  }
-}
+import Layout from "./components/Layout";
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout />, app);
--- src/main/frontend/src/js/components/Header/Title.js
+++ src/main/frontend/src/js/components/Header/Title.js
@@ -1,10 +1,7 @@
-import React from "react";
-
+import React from 'react';
 
 export default class Title extends React.Component {
   render() {
-    return (
-      <h1>{this.props.title}</h1>
-    );
+    return <h1>Welcome !</h1>;
   }
 }
