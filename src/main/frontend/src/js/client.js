4d70a524a3a5780b9427ffd99181d6b3217500d0
Marcel Widmer <marzelwidmer@gmail.com>
2017-05-27 09:53:03 +0200

    update documentation

--- src/main/frontend/src/js/client.js
+++ src/main/frontend/src/js/client.js
@@ -2,7 +2,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import Layout from "./components/Layout";
+//import Layout from "./components/Layout";
+class Layout extends React.Component {
+	render(){
+		return(
+			<h1>It works!</h1>
+			);
+	}
+}
 
 const app = document.getElementById('app');
 ReactDOM.render(<Layout/>, app);
